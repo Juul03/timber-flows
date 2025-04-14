a57f460f342f002952f7fe30589beec3af262d9f
Juul03 <juliahoek16@gmail.com>
2025-04-14 17:40:22 +0200

    function-find-unique-values

--- src/lib/scripts/formatData.js
+++ src/lib/scripts/formatData.js
@@ -1,7 +1,35 @@
-// export function formatData(data) {
-//     return data.map(item => {
-//       return {
-//         newKey: item.name,
-//       };
-//     });
-//   }
+export const formatData = (data) => {
+    return data.map(item => {
+        return {
+        keyCode: item.Keycode,
+        location: item.Location,
+        individualOrCluster: item['Individual/Cluster'],
+        length: item.Length,
+        startYear: item.StartYear,
+        endYear: item.EndYear,
+        sapwood: item.Sapwood,
+        WK: item.WK,
+        fellingDate: item['Felling date'],
+        TBP: item.TBP,
+        Gl: item.Gl,
+        SL: item.SL,
+        reference: item.Reference,
+        provenance: item.Provenance,
+        minValueMM: item['Min.Value mm'],
+        maxValueMM: item['Max.Value mm'],
+        meanValueMM: item['Mean Value mm'],
+        stdDevMm: item['Std.Dev. Mm'],
+        };
+    });
+}
+
+export const getUniqueValues = (data, key) => {
+    const values = data
+        .map(item => item[key])
+        .filter(value => value !== "");
+        
+    const uniqueSet = new Set(values);
+    const uniqueArray = Array.from(uniqueSet);
+    const sortedArray = uniqueArray.sort();
+    return sortedArray ;
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,15 +2,31 @@
 
 <div>
 <h2>Eerste Model:</h2>
-    <p><strong>Naam:</strong> {firstModel.Length}</p>
+    <p><strong>Provenance:</strong> {firstModel.provenance}</p>
 </div>
+
+
 
 <script>
     import 'bootstrap/dist/css/bootstrap.min.css';
-    import halfModels from '$lib/data/artworks/half-models.json';
-    // import { formatData } from '$lib/scripts/formatData.js';
 
-    // let formattedData = formatData(halfModels);
+    // Data files
+    import dataHalfModels from '$lib/data/artworks/half-models.json';
+
+    // Scripts
+    import { formatData, getUniqueValues } from '$lib/scripts/formatData.js';
+
+    // Components
+    // import Filters from '$lib/components/filters.svelte';
+
+    // Format data files
+    let formattedDataHalfModels = formatData(dataHalfModels);
+    let halfModels = formattedDataHalfModels;
+
+    // Get all provenances
+    let uniqueProvenances = getUniqueValues(halfModels, 'provenance');
+    console.log(uniqueProvenances);
+    console.log(halfModels);
   
     let firstModel = halfModels[0];
 </script>
