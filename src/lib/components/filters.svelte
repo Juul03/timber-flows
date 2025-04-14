eb00a2fb23b764ed21a0df6bc4669b1f1f93f20e
Juul03 <juliahoek16@gmail.com>
2025-04-14 20:44:30 +0200

    scss installation

--- src/lib/components/filters.svelte
+++ src/lib/components/filters.svelte
@@ -1,19 +1,60 @@
 <div class="container">
     <div class="row">
-        <div class="col">
-            <h2>Filters</h2>
-        </div>
-        <div class="col-2">
-            <select class="form-select">
-                <option value="">All purposes</option>
+        <div class="col-4 d-flex gap-2 align-items-center">
+            <p class="m-0 w-100">Select purpose:</p>
+            <select class="form-select" bind:value={selectedWoodPurpose}>
+                <option value="all" selected>All</option>
                 {#each woodPurposes as purpose}
                     <option value={purpose}>{purpose}</option>
                 {/each}
             </select>
         </div>
+
+        {#if selectedWoodPurpose !== "all"}
+            <div class="col-3">
+                <select class="form-select" bind:value={selectedType}>
+                    <option value="all" selected>All</option>
+                    {#each getTypes(selectedWoodPurpose) as type}
+                        <option value={type}>{type}</option>
+                    {/each}
+                </select>
+            </div>
+        {/if}
+
+        {#if selectedWoodPurpose !== "all" && selectedType !== "all"}
+            <div class="col-3">
+                <select class="form-select" bind:value={selectedSubType}>
+                    <option value="all" selected>All</option>
+                    {#each getSubTypes(selectedWoodPurpose, selectedType) as subType}
+                        <option value={subType}>{subType}</option>
+                    {/each}
+                </select>
+            </div>
+        {/if}
     </div>
 </div>
 
 <script>
-    export let woodPurposes = [];
+    export let dataWoodPurposes = [];
+    export let selectedWoodPurpose = "all";
+    export let selectedType = "all";
+    export let selectedSubType = "all";
+
+    const woodPurposes = Object.keys(dataWoodPurposes);
+
+    // Get types based on selected purpose
+    const getTypes = (purpose) => {
+        if(purpose != "all") {
+            return dataWoodPurposes[purpose]?.types || [];
+        }
+        return [];
+    };
+
+    // Get Subtypes based on selected type
+    const getSubTypes = (purpose, type) => {
+        if (purpose !== "all" && type !== "all") {
+            return dataWoodPurposes[purpose]?.subTypes[type] || [];
+        }
+        return [];
+    };
 </script>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,13 +5,20 @@
     <p><strong>Provenance:</strong> {firstModel.provenance}</p>
 </div>
 
-<Filters {woodPurposes}/>
+<Filters 
+    {dataWoodPurposes} 
+    bind:selectedWoodPurpose 
+    bind:selectedType 
+    bind:selectedSubType 
+/>
+<p>Selected purpose: {selectedWoodPurpose}{selectedType}{selectedSubType}</p>
 
 <script>
     import 'bootstrap/dist/css/bootstrap.min.css';
+    import '../assets/styles/app.scss';
 
     // Data files
-    import dataWoodPurposes from '$lib/data/woodPurposes.json'
+    import dataWoodPurposes from '$lib/data/woodPurposes.json';
     import dataHalfModels from '$lib/data/artworks/half-models.json';
 
     // Scripts
@@ -21,7 +28,10 @@
     import Filters from '$lib/components/filters.svelte';
 
     // Variables
-    const woodPurposes = Object.keys(dataWoodPurposes);
+    // Dynamic var retrieved from filters
+    let selectedWoodPurpose = "all";
+    let selectedType = "all";
+    let selectedSubType = "all";
 
     // Format data files
     let formattedDataHalfModels = formatData(dataHalfModels);
@@ -31,4 +41,7 @@
     let uniqueProvenances = getUniqueValues(halfModels, 'provenance');
   
     let firstModel = halfModels[0];
+
+    // Log the selected value
+    console.log("Selected Wood Purpose: ", selectedWoodPurpose, selectedType, selectedSubType);
 </script>
