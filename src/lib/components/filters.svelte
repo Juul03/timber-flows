65b7725435493d7bc8498bb122a9246f1995bb94
Juul03 <juliahoek16@gmail.com>
2025-04-14 19:41:50 +0200

    filter-component-dynamic-setup

--- src/lib/components/filters.svelte
+++ src/lib/components/filters.svelte
@@ -3,5 +3,17 @@
         <div class="col">
             <h2>Filters</h2>
         </div>
+        <div class="col-2">
+            <select class="form-select">
+                <option value="">All purposes</option>
+                {#each woodPurposes as purpose}
+                    <option value={purpose}>{purpose}</option>
+                {/each}
+            </select>
+        </div>
     </div>
 </div>
+
+<script>
+    export let woodPurposes = [];
+</script>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,27 @@
 <h1 class="text-primary">Timber Flows</h1>
 
 <div>
-<h2>Eerste Model:</h2>
+    <h2>Eerste Model:</h2>
     <p><strong>Provenance:</strong> {firstModel.provenance}</p>
 </div>
 
-
+<Filters {woodPurposes}/>
 
 <script>
     import 'bootstrap/dist/css/bootstrap.min.css';
 
     // Data files
+    import dataWoodPurposes from '$lib/data/woodPurposes.json'
     import dataHalfModels from '$lib/data/artworks/half-models.json';
 
     // Scripts
     import { formatData, getUniqueValues } from '$lib/scripts/formatData.js';
 
     // Components
-    // import Filters from '$lib/components/filters.svelte';
+    import Filters from '$lib/components/filters.svelte';
+
+    // Variables
+    const woodPurposes = Object.keys(dataWoodPurposes);
 
     // Format data files
     let formattedDataHalfModels = formatData(dataHalfModels);
@@ -25,8 +29,6 @@
 
     // Get all provenances
     let uniqueProvenances = getUniqueValues(halfModels, 'provenance');
-    console.log(uniqueProvenances);
-    console.log(halfModels);
   
     let firstModel = halfModels[0];
 </script>
