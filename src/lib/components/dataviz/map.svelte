5d18963cf47a31e9ba7b8fd9121d87dfb1786287
Juul03 <juliahoek16@gmail.com>
2025-05-13 11:15:56 +0200

    trade-city-custom-icon

--- src/lib/components/dataviz/map.svelte
+++ src/lib/components/dataviz/map.svelte
@@ -33,13 +33,33 @@
     let mapContainer;
     let map;
 
+    // Trade city icon
+    const createCustomIcon = (leaflet) => {
+        return leaflet.divIcon({
+            className: '',
+            html: `
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" fill="#111">
+                        <path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/>
+                    </svg>
+                </div>
+            `,
+            iconSize: [24, 24],
+            iconAnchor: [12, 24],
+            popupAnchor: [0, -24]
+        });
+    };
+
+
     // Map functions
     const addMarkersToMap = (leaflet, tradeCities, map) => {
+        const customIcon = createCustomIcon(leaflet);
+
         tradeCities.forEach(city => {
-            let marker = leaflet.marker(city.coordinates).addTo(map);
+            let marker = leaflet.marker(city.coordinates, { icon: customIcon }).addTo(map);
             marker.bindPopup(city.name).openPopup();
-        })
-    }
+        });
+    };
 
     const addProvenancesToMap = (leaflet, provenances, map) => {
         provenances.forEach(provenance => {
